8cbaeae705bb624c1c7bf091305410fa5a1bef82
Eric Feuerstein <esteinsci@gmail.com>
2021-08-28 20:12:59 -0700

    Updates

--- static/js/3.1306468c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpkdm-scout"]=this["webpackJsonpkdm-scout"]||[]).push([[3],{45:function(t,s,c){"use strict";c.r(s),s.default=c.p+"static/media/cloth.6795879b.png"}}]);
-//# sourceMappingURL=3.1306468c.chunk.js.map
---
+++ static/js/3.67326958.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpkdm-scout"]=this["webpackJsonpkdm-scout"]||[]).push([[3],{59:function(t,s,c){"use strict";c.r(s),s.default=c.p+"static/media/cloth.6795879b.png"}}]);
+//# sourceMappingURL=3.67326958.chunk.js.map
